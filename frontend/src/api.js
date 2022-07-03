6429820b47e282d1e50b7e27cae55916d263e146
Archit Choudhary <archit.digisocial@gmail.com>
2022-07-03 18:08:25 +0530

    created product details without UI

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+// IMPLEMENT ALL YOUR API REQUESTS IN THIS FILE
+import axios from 'axios';
+import { apiUrl } from './config';
+
+export const getProduct = async (id) => {
+  try {
+    const response = await axios({
+      url: `${apiUrl}/api/products/${id}`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.statusText !== 'OK') {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+// ALL THE CONFIGURATIONS OF FRONTEND WILL BE PLACED IN THIS FILE
+
+export const apiUrl = 'http://localhost:5000';
