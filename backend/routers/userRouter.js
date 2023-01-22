43d7ba6ae8bf326aed5387fdac6ab26df0880a88
Archit Choudhary <archit.digisocial@gmail.com>
2023-01-22 14:30:23 +0530

    Dummy data change

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -12,7 +12,7 @@ userRouter.get(
       const user = new User({
         name: 'admin',
         email: 'admin@example.com',
-        password: 'jsamazona',
+        password: 'archit123$$',
         isAdmin: true,
       });
       const createdUser = await user.save();
