19722c40e63cadfbca2b4891b095dcf39f831ef3
Archit Choudhary <archit.digisocial@gmail.com>
2023-02-11 00:11:39 +0530

    Changed a few parameters for deployment

--- backend/server.js
+++ backend/server.js
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
   res.status(status).send({ message: err.message });
 });
 app.listen(config.PORT, () => {
-  console.log('serve at http://localhost:5000');
+  console.log('serve at https://krazyg.cyclic.app');
 });
