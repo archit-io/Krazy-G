504e9bb73fef8a10ae1c9686af2f591b6686427a
Archit Choudhary <archit.digisocial@gmail.com>
2023-02-12 12:50:11 +0530

    Revert to Heroku Implementation: config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 // ALL THE CONFIGURATIONS OF FRONTEND WILL BE PLACED IN THIS FILE
 
-// export const apiUrl = document.location.href.startsWith('http://localhost')
-//   ? 'http://localhost:5000'
-//   : 'https://krazyg.cyclic.app';
-
-  export const apiUrl = document.location.href.startsWith('https://krazyg.cyclic.app')
-  ? 'https://krazyg.cyclic.app'
+export const apiUrl = document.location.href.startsWith('http://localhost')
+  ? 'http://localhost:5000'
   : 'https://krazyg.cyclic.app';
